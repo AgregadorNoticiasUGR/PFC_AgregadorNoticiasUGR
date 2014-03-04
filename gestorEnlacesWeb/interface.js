bd447550006494c1454aacf2f242ee3da28503ea
rdalfaraz <rdalfaraz@gmail.com>
2014-03-04 19:53:19 +0100

    Cambios en la interface del gestor de enlaces
    -Creación del menú navegador en el gestor enlaces

--- gestorEnlacesWeb/interface.js
+++ gestorEnlacesWeb/interface.js
@@ -1,28 +1,45 @@
 // JavaScript Document
 
-$(function(){
+	$(function(){
 	
-		var  tit= $("#titulo");
-		var  menu= $("#navegador");
-		var  cont= $("#contenido");
-		var portada="<form action=\"\">Usuario: <input type=\"url\" name=\"usuario\" id=\"user\"><br>Contrase&ntilde;a: <input type=\"text\" name=\"password\" id=\"pass\"><br><input type=\"submit\" value=\"Enviar\"></form>";
-		var titulo_portada="<h1>Inicio:</h1>"
-		
-		
-		function enlazarMarcador(e){
+			var  tit=  $("#titulo");
+			var  menu= $("#navegador");
+			var  cont= $("#contenido");
+			var  menu= $("#navegador");
 			
-					
-		};
-		
-		function muestra_portada(){
-			tit.html(titulo_portada);
-			cont.html(portada);
-		};
+			
+//------------Código html de cada vista de la inerface
+//
+//
 	
 	
+			//Vista inicial de portada
+			var titulo_portada="<h1>Inicio:</h1>"
+			var vista_portada="<form action=\"\">Usuario: <input type=\"url\" name=\"usuario\" id=\"user\"><br>Contrase&ntilde;a: <input type=\"text\" name=\"password\" id=\"pass\"><br><input type=\"submit\" value=\"Enviar\"></form>";
+			
+			//Vista del menu de navegación
+			//Se utilizará en todas las vistas excepto en la portada inicial
+			var menu_navegacion="<div id=\"estilo_navegador\"><ul><li class=\"separador\"><a href=\"#\" id=\"atras\"><img src=\"atras-icono.png\"></a></li><li class=\"separador\"><a href=\"#\" id=\"desconexion\"><img src=\"desconexion-icono.png\"></a></li></ul></div>";
+			
+			//Vista inicial de la lista de categorias 
+			var titulo_lista_categorias="<h1>Lista de categor&iacute;as:</h1>";
+			var vista_inicio_categoria="";
+			
+			
+			
+			function muestra_portada(){
+				tit.html(titulo_portada);
+				cont.html(vista_portada);
+			};
+			
+			function muestra_lista_categorias(){
+				tit.html(titulo_lista_categorias);
+				menu.html(menu_navegacion);
+				cont.html(vista_inicio_categoria);
+			};
 		
-		muestra_portada();
-	//	$("#inicializar").on('click',function(){ lista='';localStorage.ruta='';geolocalizar();}); 
-		//$("#recargar").on('click',function(){ geolocalizar();}); 
+		
+			
+			muestra_portada();
 		
 	});
