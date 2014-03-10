130383dce9fe4b9a6b1bd4fae8ce6648c00ca84e
rdalfaraz <rdalfaraz@gmail.com>
2014-03-11 00:32:12 +0100

    Registro de administrador funcionando
    -Cambio del fichero gestor enlaces por el nombre index
    -Programa para comprobar que un usuario con el rol de administrador esta
    registrado en el sistema. ->registro.php

--- gestorEnlacesWeb/registro.php
+++ gestorEnlacesWeb/registro.php
@@ -2,9 +2,29 @@
 <html>
 <body>
 
-<?php
-echo "PHP is fun!" ?> 
 
-
+	<?php
+        $user = $_POST["usuario"];
+        $pass = $_POST["password"];
+        
+        
+    
+    
+        $conexion = mysql_connect("127.0.0.1", "root", "");
+        mysql_select_db("enlaces_ugr_db", $conexion);
+        
+        $seleccion = "SELECT * FROM `administrador` WHERE `pass`='$pass' and `user`='$user' ;";
+        $resultado = mysql_query($seleccion, $conexion)or die(mysql_error());
+        
+        
+        $totFilas = mysql_num_rows($resultado);
+        
+        if($totFilas!=1){
+            echo "<p>No se existe ningun usuario registrado con esas caracter&iacute;sticas, compruebe datos introducidos.</p>";
+        }
+        else{
+            echo "<p>Acierto!!</p>";
+        }
+	?>
 </body>
 </html>
